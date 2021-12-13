87fd8fd15da908d184c395bb2a8b6b58cd314765
Kyle Scott <kylescott@rawintelligence.org>
2021-12-13 03:10:17 -0600

    feature:  Added compiler for yaml with variable replacement.

---
+++ docs/IAC.md
@@ -0,0 +1,13 @@
+# Infrastructure As Code Guide to EKSdivingboard
+
+## Core Concepts
+EKSdivingboard has several essential concepts to understand in order to create
+ efficient infrastructure as code projects.  We will cover each of those
+ core concepts here including:
+* Deployments
+* Deployment Definition Files
+* Default Files
+* Common Files
+* Environment Files
+
+###
--- infrastructure/infrastructure/infrastructure_stack.py
+++ infrastructure/infrastructure/infrastructure_stack.py
@@ -1,10 +1,5 @@
 from aws_cdk import core as cdk
 from compiler import StackCompiler, ConfigsCompiler
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
 
 
 class InfrastructureStack(cdk.Stack):
@@ -12,6 +7,7 @@ class InfrastructureStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, configs_root_path, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
         self.configs = None
+        self.id = construct_id
         self.defaults = None
         self.params = {**kwargs}
         self.stack = None
@@ -31,6 +27,7 @@ class InfrastructureStack(cdk.Stack):
 
     def build_stack(self):
         self.stack = StackCompiler(
+            scope=self,
+            stack_id=self.id,
             stack_configs=self.configs,
-            defaults=self.defaults
         )
